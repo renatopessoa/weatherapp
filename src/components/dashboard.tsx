0ea8897beb88490e8ad0c3d76f4a6b653fc57db7
renatopessoa <renato.fjn.si@gmail.com>
2025-04-06 14:07:53 -0300

    adiciona componentes de dashboard e utilitários de classe

---
+++ src/components/dashboard.tsx
@@ -0,0 +1,13 @@
+export default function Dashboard() {
+    return (
+        <div>
+            <h1 className="text-3xl font-bold underline">
+                Dashboard
+            </h1>
+            <p className="text-lg">
+                This is the dashboard page.
+            </p>
+            
+        </div>
+    )
+};
